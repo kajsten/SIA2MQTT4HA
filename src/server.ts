9a82149679f0e86534d446bf0f3f1f3ea6b48c4d
kajsten <45315986+kajsten@users.noreply.github.com>
2026-06-23 19:30:32 +0200

    Replace Date with now() for timestamp logging

--- src/server.ts
+++ src/server.ts
@@ -4,49 +4,48 @@ import { SIAServer } from "./sia/siaServer"
 import { handleZoneEvent } from "./handlers/ZoneEventHandler"
 import { Event } from "./events/Event"
 import { handleSystemEvent, sendInitialSystemEventState } from "./handlers/SystemEventHandler"
+import { now } from "./utils"
 
-console.log(`${Date().toLocaleString()} Starting SIA2MQTT4HA`)
+console.log(`${now()} Starting SIA2MQTT4HA`)
 
 const CONFIG_FILE = "/data/options.json"
-
 const config = getConfig(CONFIG_FILE)
 
-// Parse zones once
+console.log(`${now()} Config loaded: ${JSON.stringify(config)}`)
+
 let zones = parseZones(config)
 if (zones == null) {
-    console.log(`${Date().toLocaleString()} Couldn't parse zones, maybe there are none`)
+    console.log(`${now()} Couldn't parse zones, maybe there are none`)
 }
 
 const publisher = new Publisher(config.mqtt, zones)
 const siaServer = new SIAServer(config.sia)
 
-// Publish initial values
+setTimeout(() => {
+    const address = siaServer.server.address()
+    console.log(`${now()} Listening on: ${JSON.stringify(address)}`)
+}, 2000)
+
 sendInitialStates(publisher)
 
-// This is used only for events that contain zone details
-// it publishes to MQTT %baseTopic/zone
 siaServer.on("ZoneEvent", async function (event: Event) {
     if (zones) {
         await handleZoneEvent(event, zones, publisher)
     }
 })
 
-// This is used when there is a non-zone event
-// it publishes to MQTT $baseTopic/set|alarm|comms
 siaServer.on("SystemEvent", async function (event: Event) {
     await handleSystemEvent(event, publisher)
 })
 
-// This is used for all events
-// it publishes all raw event data to MQTT $baseTopic/event
 siaServer.on("Event", async function (event: Event) {
     await publisher.publishJSON("event", event)
 })
 
-async function sendInitialStates(publisher: Publisher){
-    try{
+async function sendInitialStates(publisher: Publisher) {
+    try {
         await sendInitialSystemEventState(publisher)
     } catch (error) {
-        console.log(`Error publishing initial states`)
+        console.log(`${now()} Error publishing initial states`)
     }
 }
