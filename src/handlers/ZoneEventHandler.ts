9e4dace6e5386b2974befc3dfbf1eeb6104ccdff
kajsten <45315986+kajsten@users.noreply.github.com>
2026-06-23 19:33:47 +0200

    Refactor ZoneEventHandler to use utility function for time

--- src/handlers/ZoneEventHandler.ts
+++ src/handlers/ZoneEventHandler.ts
@@ -1,40 +1,35 @@
 import { Event } from "../events/Event"
 import { Publisher } from "../publisher"
 import { Zones } from "../config"
+import { now } from "../utils"
 
-const stateMap: { [code: string]: { state: boolean }}={
-    "RO": {
-        state: true,
-    },
-    "RC": {
-        state: false
-    }
+const stateMap: { [code: string]: { state: boolean }} = {
+    "RO": { state: true },
+    "RC": { state: false }
 }
 
 export async function handleZoneEvent(event: Event, zones: Zones, publisher: Publisher): Promise<any> {
-    let message={}
+    let message = {}
 
-    // Get zone name
     if (!zones[parseInt(event.zone)]) {
-        console.log(`${Date().toLocaleString()} Zone does not exist in config`)
+        console.log(`${now()} Zone does not exist in config`)
         return
-    // Check if code is sane
-    } else if (!stateMap[event.code]){
-        console.log(`${Date().toLocaleString()} Invalid state`)
+    } else if (!stateMap[event.code]) {
+        console.log(`${now()} Invalid state`)
         return
     } else {
-        switch(zones[parseInt(event.zone)].type.toUpperCase()){
+        switch (zones[parseInt(event.zone)].type.toUpperCase()) {
             case "PIR":
-                message={occupancy: stateMap[event.code].state} // , time: event.time
+                message = { occupancy: stateMap[event.code].state }
                 break
             case "DOOR":
-                message={contact: stateMap[event.code].state} // , time: event.time
+                message = { contact: stateMap[event.code].state }
                 break
             default:
-                console.log(`${Date().toLocaleString()} Type is unknown`)
+                console.log(`${now()} Type is unknown`)
                 return
         }
     }
-    console.log(`${Date().toLocaleString()} ZoneEvent: ${event.zone} ${stateMap[event.code].state}`)
+    console.log(`${now()} ZoneEvent: ${event.zone} ${stateMap[event.code].state}`)
     return await publisher.publishJSON(`zone_${event.zone}`, message)
 }
